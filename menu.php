ba91f16ae24b2063a31fffc0c4e950901078f027
ambayu <am.bayu07@gmail.com>
2025-12-24 18:18:50 +0700

    Fix: Update CSP to allow cdn.datatables.net and cdn.jsdelivr.net fonts, fix menu.php img path for admin folder

--- menu.php
+++ menu.php
@@ -2,11 +2,15 @@
 $menu = $_SESSION['menu'];
 
 $level = $_SESSION['level'];
-include("koneksi.php"); ?>
+include("koneksi.php");
+
+// Detect if called from admin/ folder
+$imgPath = (strpos($_SERVER['PHP_SELF'], '/admin/') !== false) ? '../img/medan.png' : 'img/medan.png';
+?>
 
 <nav class="navbar navbar-expand-sm bg-success bg-gradient fixed-top">
     <div class="container-fluid">
-        <img src="img/medan.png" style="width:70px;">
+        <img src="<?php echo $imgPath; ?>" style="width:70px;">
         <a class="navbar-brand fw-bold lh-1 fs-6">Dinas Perpustakaan dan Kearsipan<br>Kota Medan</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
